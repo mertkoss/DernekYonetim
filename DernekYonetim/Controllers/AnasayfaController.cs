b7f008d4c9c1fe4819382cd80850b2e1d1e37230
İMİ <M@DESKTOP-FJGCIRU>
2026-02-16 23:47:44 +0300

    sayac

--- DernekYonetim/Controllers/AnasayfaController.cs
+++ DernekYonetim/Controllers/AnasayfaController.cs
@@ -15,8 +15,34 @@ namespace DernekYonetim.Controllers
         }
 
         // Üye Listesi
+        // Üye Listesi (ve Ziyaretçi Sayacı)
         public IActionResult Index()
         {
+            // --- 1. ZİYARETÇİ SAYACI İŞLEMLERİ ---
+
+            // Veritabanındaki sayaç tablosunu çağırıyoruz (DbSet adınız ZiyaretciSayacis olabilir, kontrol edin)
+            var sayac = _context.ZiyaretciSayacis.FirstOrDefault();
+
+            if (sayac == null)
+            {
+                // Eğer tabloda hiç kayıt yoksa, ilk kaydı 1 olarak oluşturuyoruz
+                sayac = new ZiyaretciSayaci { ToplamZiyaretci = 1 };
+                _context.ZiyaretciSayacis.Add(sayac);
+            }
+            else
+            {
+                // Kayıt varsa sayıyı 1 artırıyoruz (Her F5 yapıldığında artar)
+                sayac.ToplamZiyaretci++;
+            }
+
+            // Değişikliği veritabanına kaydediyoruz
+            _context.SaveChanges();
+
+            // Güncel sayıyı View tarafına gönderiyoruz
+            ViewBag.ZiyaretciSayisi = sayac.ToplamZiyaretci;
+
+
+            // --- 2. MEVCUT ÜYE LİSTESİ İŞLEMLERİ ---
             var model = new HomeViewModel
             {
                 Uyeler = _context.Uyelers.ToList()
@@ -24,6 +50,8 @@ namespace DernekYonetim.Controllers
 
             return View(model);
         }
+
+
 
         // Üye Detay
         public IActionResult Detay(int id)
