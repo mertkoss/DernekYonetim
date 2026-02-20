e91f080d368f416796ade076892ea788593a0735
İMİ <M@DESKTOP-FJGCIRU>
2026-02-20 22:31:33 +0300

    footer

---
+++ DernekYonetim/Components/FooterContactViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using DernekYonetim.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DernekYonetim.Components
+{
+    public class FooterContactViewComponent : ViewComponent
+    {
+        private readonly DernekYonetimContext _context;
+
+        public FooterContactViewComponent(DernekYonetimContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // Veritabanından iletişim bilgilerini çekiyoruz
+            var iletisim = await _context.Iletisims.FirstOrDefaultAsync();
+            return View(iletisim);
+        }
+    }
+}
